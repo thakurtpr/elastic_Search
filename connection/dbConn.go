8c41aa1e387ca2eb1492d8b4eb8f2d9b5e2578eb
Rohit Roshan <rohitroshanjena2017@gmail.com>
2024-09-20 11:44:27 +0530

    db conn

--- connection/dbConn.go
+++ connection/dbConn.go
@@ -1,7 +1,26 @@
-package connection 
+package connection
 
-import("fmt")
+import (
+	"fmt"
+	v8 "github.com/elastic/go-elasticsearch/v8"
+	// v7 "github.com/elastic/go-elasticsearch/v7"
+)
 
-func dbConn(){
-	
+var Es *v8.Client
+var err error
+
+func dbConn() {
+	cfg := v8.Config{
+		Addresses: []string{
+			"http://34.93.102.191:9200",
+		},
+		Username: "",
+		Password: "",
+	}
+
+	Es, err = v8.NewClient(cfg)
+	if err != nil {
+		fmt.Println("Client initialisation error:",err)
+	}
+
 }
--- routes/getTasks.go
+++ routes/getTasks.go
@@ -1,11 +1,23 @@
 package routes
 
 import (
+	"elastic_Search/connection"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func GetTasks(response http.ResponseWriter,request *http.Request){
 	fmt.Println("inside Get Tasks")
-
+	body := `{
+		"query": {
+			"match": { "assignee": "demo" }
+		},
+	}`
+	res, err := connection.Es.Search(
+		connection.Es.Search.WithIndex("*"),
+		connection.Es.Search.WithBody(strings.NewReader(body)),
+		connection.Es.Search.WithPretty(),
+	)
+	fmt.Println(res,err)
 }
